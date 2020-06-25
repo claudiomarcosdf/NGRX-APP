755fdc573aa045ee2f7cb344e6e47a032fc119d5
claudiomarcosdf <claudio.marcos.df@gmail.com>
2020-06-25 08:43:05 -0300

    Primeiro post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "customers",
+    loadChildren: "../app/customers/customers.module#CustomersModule"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css']
+})
+export class CustomerListComponent implements OnInit {
+  customers;
+
+  constructor(private store: Store<any>) { }
+
+  ngOnInit() {
+    this.store.dispatch({type: 'LOAD_CUSTOMERS'});
+    this.store.subscribe(state => (this.customers = state.cust.customers)); // cust vem do customers.module
+
+    console.log(this.customers);
+  }
+
+}
